3feb545497e01d9526a5da9ce9943a4263bb79f5
saikuru0 <saikuru0@protonmail.com>
2023-10-30 10:39:00 +0100

    i cannot read

--- niiso.hpp
+++ niiso.hpp
@@ -152,12 +152,12 @@ class Niiso {
 			cmds["^steam"] = {
 				"steam",
 				"generates a random steam key :evil:",
-				[this](const std::vector<std::string& args>) {
+				[this](const std::vector<std::string>& args) {
 					std::string symbols = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
 					int segments = 3+rand()%6;
 					std::string msg("");
 					for (int s=0; s<segments; s++) {
-						for (int c=0; c<5; c++) { msg += symbols[rand()%symbols.size()] }
+						for (int c=0; c<5; c++) { msg += symbols[rand()%symbols.size()]; }
 						if (s != segments-1) msg += "-";
 					}
 					_send(msg);
